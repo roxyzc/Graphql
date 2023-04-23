bbc4824a859978c9aca9471d67fca27e2fa8c8ff
roxyzc <roxyzc12@gmail.com>
2023-04-23 19:47:19 +0700

    the transaction has not been completed

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,38 +1,42 @@
-import { PrismaClient } from "@prisma/client";
-import { type Auth, type User } from "@/types";
+import { type MyContext, type Auth, type User } from "@/types";
 import { compare } from "@/utils/hash.util";
 import { generateToken, verifyToken } from "../../utils/token.util";
+import { generateNumber } from "@/utils/generateOtp.util";
+import { sendEmail, mailOptions } from "@/utils/sendEmail.util";
 
-const prisma = new PrismaClient();
-
-const register = async (values: User) => {
-  const findData = await prisma.user.findFirst({
+const register = async (values: User, context: Required<Pick<MyContext, "prisma">>) => {
+  const findData = await context.prisma.user.findFirst({
     where: {
-      OR: [
-        {
-          username: values.username,
-          email: values.email,
-        },
-        {
-          username: values.username,
-        },
-        {
-          email: values.email,
-        },
-      ],
+      OR: [{ username: values.username }, { email: values.email }],
     },
   });
 
-  if (findData === null) {
-    const data = await prisma.user.create({ data: values });
-    return data;
-  }
+  // eslint-disable-next-line @typescript-eslint/return-await
+  return context.prisma.$transaction(async (tx: any) => {
+    try {
+      if (findData === null) {
+        const data = await tx.prisma.user.create({ data: values });
+        const otp = generateNumber(6);
+        const content = await mailOptions("Verify your email", data.email, "OTP", `<h3>${otp}</h3>`);
 
-  throw new Error("User already exists");
+        const [,] = await Promise.all([
+          tx.prisma.otp.create({ data: { otp, userId: data.userId } }),
+          sendEmail(content),
+        ]).catch((err) => {
+          throw new Error(err);
+        });
+        return data;
+      }
+      throw new Error("user already exists");
+    } catch (error: any) {
+      await context.prisma.$transactionRollback(tx);
+      throw new Error(error.message);
+    }
+  });
 };
 
-const login = async (values: Auth): Promise<string | null> => {
-  const findUser = await prisma.user.findFirst({
+const login = async (values: Auth, context: Required<Pick<MyContext, "prisma">>): Promise<string | null> => {
+  const findUser = await context.prisma.user.findFirst({
     where: {
       OR: [
         {
@@ -46,6 +50,7 @@ const login = async (values: Auth): Promise<string | null> => {
   });
 
   if (findUser === null) {
+    await context.prisma.$disconnect();
     throw new Error("user not found");
   }
 
@@ -53,13 +58,13 @@ const login = async (values: Auth): Promise<string | null> => {
     throw new Error("Password not match");
   }
 
-  const token = await prisma.token.findUnique({ where: { userId: findUser.userId } });
+  const token = await context.prisma.token.findUnique({ where: { userId: findUser.userId } });
   if (token !== null) {
     return token.accessToken;
   }
 
   const { accessToken, refreshToken } = await generateToken({ id: findUser.userId, role: findUser.role });
-  await prisma.token.create({
+  await context.prisma.token.create({
     data: {
       accessToken,
       refreshToken,
@@ -70,8 +75,8 @@ const login = async (values: Auth): Promise<string | null> => {
   return accessToken;
 };
 
-const FrefreshToken = async (token: string) => {
-  const findAccessToken = await prisma.token.findFirst({
+const FrefreshToken = async (token: string, context: Required<Pick<MyContext, "prisma">>) => {
+  const findAccessToken = await context.prisma.token.findFirst({
     where: { accessToken: token },
     select: { tokenId: true, accessToken: true, refreshToken: true, user: { select: { userId: true, role: true } } },
   });
@@ -79,7 +84,7 @@ const FrefreshToken = async (token: string) => {
   const data = await verifyToken(findAccessToken.refreshToken, process.env.REFRESHTOKENSECRET as string)
     .then(async (data) => {
       const { accessToken } = await generateToken({ id: data.id, role: data.role }, false);
-      await prisma.token.update({
+      await context.prisma.token.update({
         where: {
           tokenId: findAccessToken.tokenId,
         },
@@ -92,7 +97,7 @@ const FrefreshToken = async (token: string) => {
         { id: findAccessToken.user?.userId as string, role: findAccessToken.user?.role as string },
         true
       );
-      await prisma.token.update({
+      await context.prisma.token.update({
         where: {
           tokenId: findAccessToken.tokenId,
         },
